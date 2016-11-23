1088869b05aa661fe20c239a2c2ea1785a121a9c
megkobashi <megkobashi@gmail.com>
2016-11-22 19:50:08 -0500

    final version for submission

--- a3/src/main/java/Classifier/ProfessionClassifier.java
+++ a3/src/main/java/Classifier/ProfessionClassifier.java
@@ -3,13 +3,10 @@ package Classifier;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+
 import java.util.HashMap;
-import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.SequenceFile.Writer.Option;
@@ -21,107 +18,107 @@ import org.apache.mahout.math.VectorWritable;
 import Classifier.StringIntegerList.StringInteger;
 
 public class ProfessionClassifier {
-	
-	// A map <Lemma, LemmaInfo> where LemmaInfo contains the index and number of appearances.
+
+	// A map <Lemma, LemmaInfo> where LemmaInfo contains the index and number of
+	// appearances.
 	// Index can be listed as follows: {0, 1, 2, ...}
 	private static HashMap<String, LemmaInfo> dictionary = new HashMap<String, LemmaInfo>();
 	// A map <Person's name, Professions>
 	private static HashMap<String, String[]> professionals = new HashMap<String, String[]>();
-	private static int THRESHOLD = 10;
-	
-	public static void main (String[] args) throws IOException {
+	private static int THRESHOLD = 3;
+
+	public static void main(String[] args) throws IOException {
 		Configuration conf = new Configuration();
-		System.out.println("BLAAHH");
 		loadProfessions(conf, args[0]);
 		writeSeqFile(conf, args[1], args[2]);
-		System.out.println("");
-		System.out.println(dictionary.keySet().size());
-		System.out.println("BLAAHH");
 	}
-	
+
 	// Load the list of people and their professions for the training set.
 	public static void loadProfessions(Configuration conf, String professionsFile) throws IOException {
-//        Path professionsPath = new Path (professionsFile);
-//        
-//        // Load file contents into HashMap for use by the mapper.
-//        FileSystem fs = FileSystem.get(conf);
-        BufferedReader reader = null;
-        
-        int count = 0;
+		// Path professionsPath = new Path (professionsFile);
+		//
+		// // Load file contents into HashMap for use by the mapper.
+		// FileSystem fs = FileSystem.get(conf);
+		BufferedReader reader = null;
+
 		try {
-			reader =  new BufferedReader(new FileReader(professionsFile));
+			reader = new BufferedReader(new FileReader(professionsFile));
 
 			String line;
-			
 			while ((line = reader.readLine()) != null) {
-				count++;
 				String[] tokens = line.split(" : ");
 				String professional = tokens[0];
 				String[] professions = tokens[1].split(", ");
 				professionals.put(professional, professions);
 			}
-			
+
 		} finally {
-			System.out.println("Profession line count: " + count);
 			try {
 				if (reader != null) {
 					reader.close();
 				}
-			} catch (IOException e) {}
+			} catch (IOException e) {
+			}
 		}
 	}
-	
+
 	public static void writeSeqFile(Configuration conf, String indexFile, String seqFile) throws IOException {
 
 		createDictionary(indexFile);
-		
-		FileSystem fs = FileSystem.getLocal(conf);
-		Path seqFilePath = new Path(seqFile);
-		fs.delete(seqFilePath,false);
-		
-		SequenceFile.Writer writer = null;
 
+		Path seqFilePath = new Path(seqFile);
+
+		SequenceFile.Writer writer = null;
+		BufferedReader reader = null;
 		try {
 			Option optPath = SequenceFile.Writer.file(seqFilePath);
-		    Option optKey = SequenceFile.Writer.keyClass(Text.class);
-		    Option optVal = SequenceFile.Writer.valueClass(VectorWritable.class);
-		    writer = SequenceFile.createWriter(conf, optPath, optKey, optVal);
-		    
-			List<MahoutVector> mahout_vectors = vectorize(indexFile);
-			System.out.println(" Mahout vector size: " + mahout_vectors.size());
-			for (MahoutVector mahout_vector : mahout_vectors) {
-				VectorWritable vectorWritable = new VectorWritable();
-				vectorWritable.set(mahout_vector.getVector());
-				writer.append(new Text("/" + mahout_vector.getClassifier() + "/"), vectorWritable);
+			Option optKey = SequenceFile.Writer.keyClass(Text.class);
+			Option optVal = SequenceFile.Writer.valueClass(VectorWritable.class);
+			writer = SequenceFile.createWriter(conf, optPath, optKey, optVal);
+
+			reader = new BufferedReader(new FileReader(indexFile));
+			VectorWritable vectorWritable = new VectorWritable();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				MahoutVector mahout_vector = vectorize(line);
+
+				if (mahout_vector != null) {
+					vectorWritable.set(mahout_vector.getVector());
+					writer.append(new Text("/" + mahout_vector.getClassifier() + "/"), vectorWritable);
+				}
 			}
-			
+
 		} finally {
 			try {
 				if (writer != null) {
 					writer.close();
 				}
-			} catch (IOException e) {}
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (IOException e) {
+			}
 		}
 	}
-	
-	// Parse through ARTICLE_LEMMA_INDEX and create dictionary of all lemmas appearing in corpus.
+
+	// Parse through ARTICLE_LEMMA_INDEX and create dictionary of all lemmas
+	// appearing in corpus.
 	private static void createDictionary(String indexFile) throws IOException {
-		
+
 		BufferedReader reader = null;
-		int lines = 0;
+		
 		try {
 			String line;
-			reader =  new BufferedReader(new FileReader(indexFile));
+			reader = new BufferedReader(new FileReader(indexFile));
 			// Line consists of: Person name<\t>StringIntegerList
 			while ((line = reader.readLine()) != null) {
-				lines++;
 				String[] entry = line.split("\t");
 				String person = entry[0];
-				
+
 				if (professionals.containsKey(person) && entry.length > 1) {
 					StringIntegerList lemma_freqs = new StringIntegerList();
 					lemma_freqs.readFromString(line.split("\t")[1]);
-				
+
 					for (StringInteger lemma_freq : lemma_freqs.getIndices()) {
 						String lemma = lemma_freq.getString();
 						int count = lemma_freq.getValue();
@@ -131,92 +128,54 @@ public class ProfessionClassifier {
 							lemma_info.addCount(count);
 							dictionary.put(lemma, lemma_info);
 						} else {
-							// Only update the number of appearances of the lemma.
+							// Only update the number of appearances of the
+							// lemma.
 							dictionary.get(lemma).addCount(count);
 						}
-							
+
 					}
 				}
 
 			}
 		} finally {
-			System.out.println("Index file line count: " + lines);
 			try {
 				if (reader != null) {
 					reader.close();
 				}
-			} catch (IOException e) {}
+			} catch (IOException e) {
+			}
 		}
 	}
-	
-	private static List<MahoutVector> vectorize(String indexFile) throws IOException {
-		
-		// List of vectors of a profession and lemmas associated with the profession.
-		List<MahoutVector> mahout_vectors = new ArrayList<MahoutVector>();
-		BufferedReader reader = null;
-//		FSDataInputStream stream;
 
-		int lines = 0;
-		try {
-			// Line consists of: Person name<\t>StringIntegerList
-			Configuration conf = new Configuration();
-			Path path = new Path(indexFile);
-			FileSystem fs = FileSystem.get(conf);
-//			stream = fs.open(path);
-            reader=new BufferedReader(new InputStreamReader(fs.open(path)));
+	private static MahoutVector vectorize(String line) throws IOException {
 
-			String[] entry;
-			String person;
-			String line = "";
-			while (line != null) {
-				try {
-					line = reader.readLine();
-					
-					lines++;
-					entry = line.split("\t");
-					person = entry[0];
-					if (lines % 1000 == 0) {
-						System.out.println("Line number: " + lines);
-					}
-					if (professionals.containsKey(person) && entry.length > 1) {
-						Vector vector = new RandomAccessSparseVector(dictionary.keySet().size() -1, line.length() - 1);
-						
-						StringIntegerList lemma_freqs = new StringIntegerList();
-						lemma_freqs.readFromString(entry[1]);
-						
-						// StringInteger <lemma, count>
-						for (StringInteger lemma_freq: lemma_freqs.getIndices()) {
-							LemmaInfo lemma_info = dictionary.get(lemma_freq.getString());
-							if (lemma_info.getCount() > THRESHOLD) {
-								// Set element at index specified in dictionary paired with TFIDF
-								vector.set(lemma_info.getIndex(), lemma_info.getCount());
-							}
-						}
-										
-						// Create a Mahout vector for each profession assigned to person.
-							MahoutVector mahout_vector = new MahoutVector();
-							mahout_vector.setClassifier(professionals.get(person)[0]);
-							mahout_vector.setVector(vector);
-							mahout_vectors.add(mahout_vector);
-						
-				
-					}
-				} catch (OutOfMemoryError e) {
-					line = "";
-					System.out.println("Out of memory at " + lines);
+		String[] entry = line.split("\t");
+		String person = entry[0];
+		if (professionals.containsKey(person) && entry.length > 1) {
+			Vector vector = new RandomAccessSparseVector(dictionary.keySet().size() - 1, line.length() - 1);
+
+			StringIntegerList lemma_freqs = new StringIntegerList();
+			lemma_freqs.readFromString(entry[1]);
+
+			// StringInteger <lemma, count>
+			for (StringInteger lemma_freq : lemma_freqs.getIndices()) {
+				LemmaInfo lemma_info = dictionary.get(lemma_freq.getString());
+				if (lemma_info.getCount() > THRESHOLD) {
+					// Set element at index specified in dictionary paired with
+					// TFIDF
+					vector.set(lemma_info.getIndex(), lemma_info.getCount());
 				}
-			}		
-		} finally {
-			System.out.println("Index file line count for vectorize: " + lines);
-			try {
-				if (reader != null) {
-					reader.close();
-				}
-			} catch (IOException e) {}
+			}
+
+			// Create a Mahout vector for each profession assigned to person.
+			MahoutVector mahout_vector = new MahoutVector();
+			mahout_vector.setClassifier(professionals.get(person)[0]);
+			mahout_vector.setVector(vector);
+
+			return mahout_vector;
 		}
 		
-		return mahout_vectors;
-		
+		return null;
 	}
-	
+
 }
